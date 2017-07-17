b538123c7c427546ac00ee5d2bb4bc5d4d4ab5c2
Nick Tomlin <nick.tomlin@gmail.com>
2017-07-17 18:17:46 -0500

    fix: Textile converter is no longer bundled with default Jekyll install

--- lib/octopress-quote-tag/utils.rb
+++ lib/octopress-quote-tag/utils.rb
@@ -6,24 +6,12 @@ module Octopress
           path  = context.environments.first['page']['path']
           ext   = File.extname(path[1..-1])[1..-1]
           site  = context.registers[:site]
-          mdext = site.config['markdown_ext']
-          txext = site.config['textile_ext']
 
-          markdown_converter = if site.respond_to?(:find_converter_instance)
-            site.find_converter_instance(Jekyll::Converters::Markdown)
-          else
-            site.getConverterImpl(Jekyll::Converters::Markdown)
-          end
-
-          textile_converter = if site.respond_to?(:find_converter_instance)
-            site.find_converter_instance(Jekyll::Converters::Textile)
-          else
-            site.getConverterImpl(Jekyll::Converters::Textile)
-          end
-
-          if mdext.include? ext
+          if site.config['markdown_ext'].include?(ext)
+            markdown_converter = site.find_converter_instance(Jekyll::Converters::Markdown)
             markdown_converter.convert(content)
-          elsif txext.include? ext
+          elsif defined?(Jekyll::TextileConverter) && site.config['textile_ext'].include?(ext) &&
+            textile_converter = site.find_converter_instance(Jekyll::TextileConverter)
             textile_converter.convert(content)
           else
             "<p>" + content.strip.gsub(/\n\n/, "<p>\n\n</p>") + "</p>"
--- lib/octopress-quote-tag/version.rb
+++ lib/octopress-quote-tag/version.rb
@@ -1,7 +1,7 @@
 module Octopress
   module Tags
     module Quote
-      VERSION = "1.0.4"
+      VERSION = "2.0.0"
     end
   end
 end
